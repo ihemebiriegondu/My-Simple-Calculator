743dd39b9733210a67e4fb99128dfb046220aca1
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-06-02 21:04:09 +0100

    .

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,31 @@
 let toggleMode = document.getElementById("toggle-icon");
-let btnMode = document.querySelector(".btn");
-
 
 
 function toggler() {
-    var change = document.body;
-    var darkBtn = document.querySelector(".btn")
+    let change = document.body;
+    let darkBtn = document.querySelectorAll(".btn");
+    let darkKeyboard = document.getElementById("keyboard");
+    let diffColor = document.querySelectorAll(".diff-color");
+    let firstRow = document.querySelector(".row");
+
+
 
     change.classList.toggle("dark-mode");
-    darkBtn.classList.toggle("dark-mode");
+    darkBtn.forEach(button => {
+        button.classList.toggle("dark-mode-btn");
+    });
+    diffColor.forEach(color => {
+        color.classList.toggle("diff-color-dark-mode-btn");
+    });
+
+    if (change.classList.contains("dark-mode")) {
+        toggleMode.classList.add("fa-sun");
+        darkKeyboard.style.backgroundColor = "rgb(35, 35, 35)";
+        firstRow.style.backgroundColor = "rgb(35, 35, 35)";
+    } else {
+        toggleMode.classList.remove("fa-moon");
+        toggleMode.classList.add("fa-sun")
+        darkKeyboard.style.backgroundColor = "white";
+        firstRow.style.backgroundColor = "white";
+    }
 }
